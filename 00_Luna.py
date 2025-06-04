59026241c5c89b52c5542de138909122ec2b33ae
lunachiara <lunachiarapirito@gmail.com>
2025-06-03 21:12:26 -0300

    Update 00_Luna.py

--- 00_Luna.py
+++ 00_Luna.py
@@ -1,24 +1,6 @@
-# Clase en vídeo: https://youtu.be/Kp4Mvapo5kc
+print('Hola, me llamo Luna')
 
-### Hola Mundo ###
-
-# Nuestro hola mundo en Python
-print("Hola Python")
-print('Hola Python')
-
-# Esto es un comentario
-
-"""
-Este es un
-comentario
-en varias líneas
-"""
-
-'''
-Este también es un
-comentario
-en varias líneas
-'''
+# Comentario
 
 # Cómo consultar el tipo de dato
 print(type("Soy un dato str"))  # Tipo 'str'
