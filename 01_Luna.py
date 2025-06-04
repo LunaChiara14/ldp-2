3c13529b63eb1823cc68955fc29f6117d2ebcfa3
lunachiara <lunachiarapirito@gmail.com>
2025-06-03 21:14:17 -0300

    01_Luna.py

--- 01_Luna.py
+++ 01_Luna.py
@@ -1,5 +1,3 @@
-# Clase en vídeo: https://youtu.be/Kp4Mvapo5kc?t=2938
-
 ### Variables ###
 
 my_string_variable = "My String variable"
@@ -23,7 +21,7 @@ print("Este es el valor de:", my_bool_variable)
 print(len(my_string_variable))
 
 # Variables en una sola línea. ¡Cuidado con abusar de esta sintaxis!
-name, surname, alias, age = "Brais", "Moure", 'MoureDev', 35
+name, surname, alias, age = "Luna", "Pirito", 'LunaChiara14', 17
 print("Me llamo:", name, surname, ". Mi edad es:",
       age, ". Y mi alias es:", alias)
 
@@ -34,14 +32,14 @@ print(name)
 print(age)
 
 # Cambiamos su tipo
-name = 35
-age = "Brais"
+name = 17
+age = "Luna"
 print(name)
 print(age)
 
 # ¿Forzamos el tipo?
 address: str = "Mi dirección"
 address = True
-address = 5
-address = 1.2
+address = 28
+address = 2.8
 print(type(address))
